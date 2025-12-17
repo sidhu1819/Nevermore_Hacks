643f36d3463b2e501a6e02a360e55849c75d4b5d
sidhu1819 <832SIDDHARTHA@GMAIL.COM>
2025-12-17 22:23:33 +0530

    them change

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-// Set the date we're counting down to (Oct 27, 2025 at Midnight)
-var countDownDate = new Date("Oct 27, 2025 23:59:59").getTime();
+// Set the date to Dec 27th, 2025 at 11:59:59 PM
+var countDownDate = new Date("Dec 27, 2025 23:59:59").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
-
-  // Get today's date and time
   var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in the element with id="countdown"
-  document.getElementById("countdown").innerHTML = 
-  days + "d : " + hours + "h : " + minutes + "m : " + seconds + "s ";
+  // Update elements
+  if(document.getElementById("d")) {
+      document.getElementById("d").innerText = days < 10 ? '0'+days : days;
+      document.getElementById("h").innerText = hours < 10 ? '0'+hours : hours;
+      document.getElementById("m").innerText = minutes < 10 ? '0'+minutes : minutes;
+      document.getElementById("s").innerText = seconds < 10 ? '0'+seconds : seconds;
+  }
 
-  // If the count down is over, write some text 
+  // When countdown ends
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPLODED (CLOSED)";
-    document.getElementById("countdown").style.color = "red";
+    if(document.querySelector(".timer-box")) {
+        document.querySelector(".timer-box").innerHTML = "<h2 style='color:red'>REGISTRATION CLOSED</h2>";
+    }
   }
 }, 1000);
